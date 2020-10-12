b0b45cbd0ea96ce1150a8733d6d387abf7259217
Michał Sidor <public+git@meekchopp.es>
2020-10-12 22:02:44 +0200

    Add early scaffolding for periodic updating

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
+#![feature(never_type)]
+
 use warp::Filter;
 
 pub(crate) mod database;
 pub(crate) mod templates;
 mod views;
+
+const UPDATE_PERIOD: tokio::time::Duration = tokio::time::Duration::from_secs(5);
 
 #[tokio::main]
 async fn main() {
@@ -21,5 +25,22 @@ async fn main() {
         .and_then(views::fetch_object);
     let routes = index.or(fetch);
 
+    tokio::spawn(async move {
+        periodic_updater(pool).await.unwrap();
+    });
+
     warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
+}
+
+async fn periodic_updater(db: database::Db) -> anyhow::Result<!> {
+    let mut interval = tokio::time::interval(UPDATE_PERIOD);
+    loop {
+        {
+            let conn = db.get()?;
+            for feed in database::list_feeds(conn)? {
+                println!("{:?}", feed.last_feed()?);
+            }
+        }
+        interval.tick().await;
+    }
 }
